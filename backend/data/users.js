bc2643949add3d5258f326848cd9dfe14a5dc193
Development-Person <ibaemail@gmail.com>
2020-11-03 11:46:23 +1100

    Preparing sample data

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs';
+
+/* usually we would hash the password asyncronously but for this example we will just do syncronously*/
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+];
+
+export default users;
