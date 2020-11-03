19bcbd59fc154e90cd8c5fd23a5fcdd7edf93d06
Development-Person <ibaemail@gmail.com>
2020-11-03 15:30:40 +1100

    Get routes for products and product
    Also renamed ProductScreen from jsx to js for consistency. Nothing else changed.

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -14,6 +14,7 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+/* these were used when we were fetching from the producst.js file. These are no longer used since we are fetching from mongoDB.
 app.get('/api/products', (req, res) => {
   res.json(products);
 });
@@ -22,6 +23,9 @@ app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
+*/
+
+app.use('/api/products', productRoutes);
 
 //dotenv allows you to do things like set ports, etc. Dotenv should be secret and not pushed to github etc.
 const PORT = process.env.PORT || 5000;
