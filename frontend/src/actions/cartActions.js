360f86b6317c4c560765d96b3da03a348d5e52a9
Development-Person <ibaemail@gmail.com>
2020-11-08 08:52:10 +1100

    Cart Screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -10,10 +10,10 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       name: data.name,
       image: data.image,
       price: data.price,
-      countInstock: data.countInstock,
-      qty,
+      countInStock: data.countInStock,
+      qty: qty,
     },
   });
-  localStorage.setItem('cartitems', JSON.stringify(getState().cart.cartItems)); //this saves the cart in local storage so that people can come back and buy later.
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems)); //this saves the cart in local storage so that people can come back and buy later.
   //getstate gives us a JS object so we want to stringify it, because we can only save strings in local storage.
 };
