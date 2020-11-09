08e2194b30bdde6bfba02fd91cf4d632de26d846
Development-Person <ibaemail@gmail.com>
2020-11-09 20:45:57 +1100

    Custom Auth Middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET); //jwt.verify takes in a token and the secret and decodes the information in the token
+      //console.log(decoded); //this logs the decoded information (which includes the user ID but no other user information, as that's all we put in when we generated the token using generateToken.js)
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error('Not authorised, token failed');
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorised, no token');
+  }
+});
+
+export { protect };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser); //we use post because we are only posting, so we don't have to user router.route().post. we use /login because it's being hooked to api/users.
+router.route('/profile').get(protect, getUserProfile); //to implement middleware we just add it as the first argument to the request (protect is the middleware)
 
 export default router;
