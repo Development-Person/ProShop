9e6896aab60730cf5433fc67683cdaf54bd5067d
Development-Person <ibaemail@gmail.com>
2020-11-11 10:45:48 +1100

    Update Profile Endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,15 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  updateUserProfile,
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').post(registerUser); // this is a '/' because user routes is added to the server as /app/users, so if we are trying to post to users we are already there, at root.
 router.post('/login', authUser); //we use post because we are only posting, so we don't have to user router.route().post. we use /login because it's being hooked to api/users.
-router.route('/profile').get(protect, getUserProfile); //to implement middleware we just add it as the first argument to the request (protect is the middleware)
+router
+  .route('/profile')
+  .get(protect, getUserProfile) //to implement middleware we just add it as the first argument to the request (protect is the middleware)
+  .put(protect, updateUserProfile); //because it's a route we can just chain get and put requests, which are only activated when called
 
 export default router;
